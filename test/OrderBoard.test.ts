573db40d50e9cc2a4e37d71300118a1444e57075
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-18 12:21:32 +0000

    Refactor to use helpers

--- test/OrderBoard.test.ts
+++ test/OrderBoard.test.ts
@@ -1,26 +1,7 @@
-import Order from "../src/Order";
 import OrderBoard from "../src/OrderBoard";
-import OrderId from "../src/OrderId";
-import OrderType from "../src/OrderType";
-import Price from "../src/Price";
-import Quantity from "../src/Quantity";
-import UserId from "../src/UserId";
+import { myOrder, myOtherOrder } from "./helpers/helpers";
 
 describe("OrderBoard", () => {
-  const myUserId: UserId = { userId: 1 };
-  const myQuantity: Quantity = { quantity: 3.5, unit: "kg" };
-  const myPrice: Price = { price: 306, currency: "GBP" };
-  const myOrderType: OrderType = { orderType: "SELL" };
-  const myOrderId: OrderId = { orderId: 1 };
-  const myOrder: Order = new Order(myUserId, myQuantity, myPrice, myOrderType, myOrderId);
-
-  const myOtherUserId: UserId = { userId: 2 };
-  const myOtherQuantity: Quantity = { quantity: 1.2, unit: "kg" };
-  const myOtherPrice: Price = { price: 310, currency: "GBP" };
-  const myOtherOrderType: OrderType = { orderType: "SELL" };
-  const myOtherOrderId: OrderId = { orderId: 2 };
-  const myOtherOrder: Order = new Order(myUserId, myQuantity, myPrice, myOrderType, myOrderId);
-
   describe(".register", () => {
     it("registers a new order", () => {
       OrderBoard.register(myOrder);
