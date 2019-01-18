b05e590c7f855f0b01a482861ca599492a810ce1
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-18 13:26:11 +0000

    Add tests for SummaryItem

--- test/SummaryItem.test.ts
+++ test/SummaryItem.test.ts
@@ -1,8 +1,16 @@
 import SummaryItem from "../src/SummaryItem";
-import { mySummaryItem, mySummaryQuantity } from "./helpers/helpers";
+import { mySummaryItem, mySummaryOrderType, mySummaryPrice, mySummaryQuantity } from "./helpers/helpers";
 
 describe("SummaryItem", () => {
   it("stores the total quantity", () => {
     expect(mySummaryItem.quantity).toEqual(mySummaryQuantity);
+  });
+
+  it("stores the price", () => {
+    expect(mySummaryItem.price).toEqual(mySummaryPrice);
+  });
+
+  it("stores the order type", () => {
+    expect(mySummaryItem.orderType).toEqual(mySummaryOrderType);
   });
 });
