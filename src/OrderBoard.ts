4d67271cfbfd4d565aa1a6e97037823f72d36779
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-18 11:47:39 +0000

    Add passing test for .cancel plus small refactorings

--- src/OrderBoard.ts
+++ src/OrderBoard.ts
@@ -3,7 +3,11 @@ import Order from "./Order";
 export default class OrderBoard {
   public static orders: Order[] = [];
 
-  public static register(order: Order) {
-    OrderBoard.orders.push(order);
+  public static register(orderToRegister: Order) {
+    OrderBoard.orders.push(orderToRegister);
+  }
+
+  public static cancel(orderToCancel: Order) {
+    OrderBoard.orders = OrderBoard.orders.filter((order) => order !== orderToCancel);
   }
 }
--- test/OrderBoard.test.ts
+++ test/OrderBoard.test.ts
@@ -7,17 +7,24 @@ import Quantity from "../src/Quantity";
 import UserId from "../src/UserId";
 
 describe("OrderBoard", () => {
-  describe(".register", () => {
-    const myUserId: UserId = { userId: 1 };
-    const myQuantity: Quantity = { quantity: 3.5, unit: "kg" };
-    const myPrice: Price = { price: 306, currency: "GBP" };
-    const myOrderType: OrderType = { orderType: "SELL" };
-    const myOrderId: OrderId = { orderId: 1 };
-    const myOrder: Order = new Order(myUserId, myQuantity, myPrice, myOrderType, myOrderId);
+  const myUserId: UserId = { userId: 1 };
+  const myQuantity: Quantity = { quantity: 3.5, unit: "kg" };
+  const myPrice: Price = { price: 306, currency: "GBP" };
+  const myOrderType: OrderType = { orderType: "SELL" };
+  const myOrderId: OrderId = { orderId: 1 };
+  const myOrder: Order = new Order(myUserId, myQuantity, myPrice, myOrderType, myOrderId);
 
+  describe(".register", () => {
     it("registers a new order", () => {
       OrderBoard.register(myOrder);
       expect(OrderBoard.orders).toContain(myOrder);
+    });
+  });
+
+  describe(".cancel", () => {
+    it("cancels an existing order", () => {
+      OrderBoard.cancel(myOrder);
+      expect(OrderBoard.orders).not.toContain(myOrder);
     });
   });
 });
