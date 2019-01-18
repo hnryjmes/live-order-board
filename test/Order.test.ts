b611d4353db4d2cc789eac5d085c311a355992ca
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-18 10:10:53 +0000

    Refactor test to use beforeEach

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,20 +1,22 @@
 import Order from "../src/Order";
 
 describe("Order", () => {
+  let order: Order;
+
+  beforeEach(() => {
+    order = new Order(1, 3.5, 306, "SELL");
+  });
+
   it("stores the userId", () => {
-    const order = new Order(1, 3.5, 306, "SELL");
     expect(order.userId).toBe(1);
   });
   it("stores the quantity", () => {
-    const order = new Order(1, 3.5, 306, "SELL");
     expect(order.quantity).toBe(3.5);
   });
   it("stores the price", () => {
-    const order = new Order(1, 3.5, 306, "SELL");
     expect(order.price).toBe(306);
   });
   it("stores the orderType", () => {
-    const order = new Order(1, 3.5, 306, "SELL");
     expect(order.orderType).toBe("SELL");
   });
 });
