0ed50f23b3473e43d99ed9349f62a00d7df1920e
Henry Cooksley <henry.cooksley@gmail.com>
2019-01-18 11:36:43 +0000

    Fix OrderBoard test

--- test/OrderBoard.test.ts
+++ test/OrderBoard.test.ts
@@ -1,5 +1,6 @@
 import Order from "../src/Order";
 import OrderBoard from "../src/OrderBoard";
+import OrderId from "../src/OrderId";
 import OrderType from "../src/OrderType";
 import Price from "../src/Price";
 import Quantity from "../src/Quantity";
@@ -11,7 +12,8 @@ describe("OrderBoard", () => {
     const myQuantity: Quantity = { quantity: 3.5 };
     const myPrice: Price = { price: 306 };
     const myOrderType: OrderType = { orderType: "SELL" };
-    const myOrder: Order = new Order(myUserId, myQuantity, myPrice, myOrderType);
+    const myOrderId: OrderId = { orderId: 1 };
+    const myOrder: Order = new Order(myUserId, myQuantity, myPrice, myOrderType, myOrderId);
 
     it("registers a new order", () => {
       OrderBoard.register(myOrder);
